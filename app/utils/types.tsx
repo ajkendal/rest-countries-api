6fdf140db14a4468658854986f8943cd2e8415cb
Amanda J Kendal-Brown <15990675+ajkendal@users.noreply.github.com>
2025-09-26 14:28:21 -0700

    Expand Country type with additional fields
    Added tld, alpha3Code, and borders properties to the Country type for improved data representation. Also removed semicolons for consistency in formatting.

--- app/utils/types.tsx
+++ app/utils/types.tsx
@@ -1,17 +1,20 @@
 export type Country = {
   name: {
-    common: string;
-    official?: string;
-    nativeName?: Record<string, { official: string; common: string }>;
-  };
-  cca2?: string;
-  cca3?: string;
-  region?: string;
-  subregion?: string;
-  population?: number;
-  capital?: string[];
-  languages?: Record<string, string>;
-  currencies?: Record<string, { name: string; symbol: string }>;
-  flags?: { png?: string; svg?: string };
-  [key: string]: unknown; // If truly needed for dynamic properties
+    common: string
+    official?: string
+    nativeName?: Record<string, { official: string; common: string }>
+  }
+  cca2?: string
+  cca3?: string
+  tld?: string[]
+  alpha3Code?: string
+  borders?: string[]
+  region?: string
+  subregion?: string
+  population?: number
+  capital?: string[]
+  languages?: Record<string, string>
+  currencies?: Record<string, { name: string; symbol: string }>
+  flags?: { png?: string; svg?: string }
+  [key: string]: unknown
 }
